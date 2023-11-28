7a444cdf82f370eceec4aa71dde66249c54daa30
sahankumara <127411663+sahankumara@users.noreply.github.com>
2023-11-28 12:19:44 +0530

    Act 2

---
+++ Activity2G/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Activity2G
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMarkSubject1 = new System.Windows.Forms.TextBox();
+            this.txtMarkSubject2 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.labelMark = new System.Windows.Forms.Label();
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Subject1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Subject2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Average";
+            // 
+            // txtMarkSubject1
+            // 
+            this.txtMarkSubject1.Location = new System.Drawing.Point(113, 34);
+            this.txtMarkSubject1.Name = "txtMarkSubject1";
+            this.txtMarkSubject1.Size = new System.Drawing.Size(100, 20);
+            this.txtMarkSubject1.TabIndex = 3;
+            this.txtMarkSubject1.Text = "0";
+            this.txtMarkSubject1.Validating += new System.ComponentModel.CancelEventHandler(this.txtMarkSubject1_Validating);
+            // 
+            // txtMarkSubject2
+            // 
+            this.txtMarkSubject2.Location = new System.Drawing.Point(113, 73);
+            this.txtMarkSubject2.Name = "txtMarkSubject2";
+            this.txtMarkSubject2.Size = new System.Drawing.Size(100, 20);
+            this.txtMarkSubject2.TabIndex = 4;
+            this.txtMarkSubject2.Text = "0";
+            this.txtMarkSubject2.TextChanged += new System.EventHandler(this.txtMarkSubject2_TextChanged);
+            this.txtMarkSubject2.Validating += new System.ComponentModel.CancelEventHandler(this.txtMarkSubject1_Validating);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(113, 102);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 20);
+            this.txtResult.TabIndex = 5;
+            this.txtResult.Text = "0";
+            // 
+            // BtnSubmit
+            // 
+            this.BtnSubmit.Location = new System.Drawing.Point(113, 146);
+            this.BtnSubmit.Name = "BtnSubmit";
+            this.BtnSubmit.Size = new System.Drawing.Size(100, 23);
+            this.BtnSubmit.TabIndex = 6;
+            this.BtnSubmit.Text = "Submit";
+            this.BtnSubmit.UseVisualStyleBackColor = true;
+            this.BtnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // labelMark
+            // 
+            this.labelMark.AutoSize = true;
+            this.labelMark.Location = new System.Drawing.Point(229, 102);
+            this.labelMark.Name = "labelMark";
+            this.labelMark.Size = new System.Drawing.Size(0, 13);
+            this.labelMark.TabIndex = 7;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 219);
+            this.Controls.Add(this.labelMark);
+            this.Controls.Add(this.BtnSubmit);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtMarkSubject2);
+            this.Controls.Add(this.txtMarkSubject1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aggregate Function-Average";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMarkSubject1;
+        private System.Windows.Forms.TextBox txtMarkSubject2;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button BtnSubmit;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label labelMark;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+    }
+}
+
---
+++ Activity2G/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity2G
+{
+    public partial class Form1 : Form
+    {
+        private int MarkSubject1;
+        private int MarkSubject2;
+        
+
+        private int intValidation;
+
+        private bool bSubject1 = false;
+        private bool bSubject2 = false;
+      
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            MarkSubject1 = 0;
+            MarkSubject2 = 0;
+
+            txtResult.ReadOnly = true;
+        }
+
+        private void txtMarkSubject1_Validating(object sender, CancelEventArgs e)
+        {
+            errorProvider1.SetError(txtMarkSubject1, "");
+            bSubject1 = false;
+            if (!int.TryParse(txtMarkSubject1.Text, out intValidation))
+            {
+                bSubject1 = true;
+                errorProvider1.SetError(txtMarkSubject1, "Please fill the required field");
+            }
+
+
+        }
+
+        
+
+        private void txtMarkSubject2_TextChanged(object sender, EventArgs e)
+        {
+            errorProvider2.SetError(txtMarkSubject2, "");
+            bSubject2 = false;
+            if (!int.TryParse(txtMarkSubject2.Text, out intValidation))
+            {
+                bSubject2 = true;
+                errorProvider2.SetError(txtMarkSubject2, "Please fill the required field");
+            }
+
+        }
+      
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (bSubject1 == false && bSubject2 == false)
+                {
+                    MarkSubject1 = int.Parse(txtMarkSubject1.Text);
+                    MarkSubject2 = int.Parse(txtMarkSubject2.Text);
+
+                    result = ((MarkSubject1 + MarkSubject2) / 2);
+
+                    txtResult.Text = result.ToString();
+
+                    if (result >= 0 && result <= 40)
+                    {
+                        labelMark.Text = "Grade F";
+
+                    }
+                    else if (result >= 40 && result <= 65)
+                    {
+                        labelMark.Text = "Grade C";
+
+                    }
+                    else if (result >= 65 && result <= 75)
+                    {
+                        labelMark.Text = "Grade B";
+
+                    }
+                    else if (result >= 75 && result <= 100)
+                    {
+                        labelMark.Text = "Grade A";
+
+                    }
+
+
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+
+        }
+    }
+}
